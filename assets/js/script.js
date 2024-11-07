8325d036e40b8fec19357f7ea191cab34e780b87
Visw3108 <144777277+Visw3108@users.noreply.github.com>
2024-11-07 11:58:17 +0530

    new

--- assets/js/script.js
+++ assets/js/script.js
@@ -195,7 +195,31 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 
+document.addEventListener('contextmenu', function(e) {
+  e.preventDefault();
+});
 
+document.addEventListener('selectstart', function(e) {
+  e.preventDefault();
+});
+
+document.addEventListener('copy', function(e) {
+  e.preventDefault();
+});
+
+document.addEventListener('cut', function(e) {
+  e.preventDefault();
+});
+
+document.addEventListener('paste', function(e) {
+  e.preventDefault();
+});
+
+document.addEventListener('keydown', function(e) {
+  if (e.ctrlKey && (e.key === 'c' || e.key === 'x' || e.key === 'v')) {
+    e.preventDefault();
+  }
+});
 
 
 
